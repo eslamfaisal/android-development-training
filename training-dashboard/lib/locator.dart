931a1fc51cae7f9027a7b384a076a4bf359f134e
eslamfaisal <eslam.faisal@algodriven.xyz>
2024-02-20 14:29:03 +0200

    init project structure

--- training-dashboard/lib/locator.dart
+++ training-dashboard/lib/locator.dart
@@ -1,5 +1,6 @@
 
 import 'package:get_it/get_it.dart';
+import 'package:training_questions_form/screens/splash/view_model/splash_view_model.dart';
 
 import 'services/navigation_service.dart';
 
@@ -7,4 +8,5 @@ GetIt locator = GetIt.instance;
 
 void setupLocator() {
   locator.registerLazySingleton(() => NavigationService());
+  locator.registerLazySingleton(() => SplashViewModel());
 }
