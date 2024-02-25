682283921713f2e35a9bc7e3f251dc2e1b8ce503
eslamfaisal <eslam.faisal@algodriven.xyz>
2024-02-25 13:37:47 +0200

    finish splash screen for android above and below 12

--- ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/MainActivity.kt
+++ ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-//        initSplashScreen()
+        initSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
