47318611dc17ac0554aac6b6ced9358c8e060c56
eslamfaisal <eslam.faisal@algodriven.xyz>
2024-03-02 02:52:02 +0200

    use datastore keys

--- ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/data/repository/user/UserPreferencesRepository.kt
+++ ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/data/repository/user/UserPreferencesRepository.kt
@@ -1,22 +1,14 @@
 package com.training.ecommerce.data.repository.user
 
 import android.content.Context
-import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import com.training.ecommerce.data.datasource.datastore.DataStoreKeys.IS_USER_LOGGED_IN
 import com.training.ecommerce.data.datasource.datastore.dataStore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.runBlocking
 
 
 class UserPreferencesRepository(private val context: Context) {
-
-    // Define keys for your preferences
-    companion object {
-        val IS_USER_LOGGED_IN = booleanPreferencesKey("is_user_logged_in")
-    }
 
     // Read from DataStore
     val isUserLoggedIn: Flow<Boolean> = context.dataStore.data
