58c12bbc924e83b487018265a06b16a312cde2d3
eslamfaisal <eslam.faisal@algodriven.xyz>
2024-03-18 12:09:28 +0200

    fix datastore init

--- ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/data/datasource/datastore/UserPreferencesDataSource.kt
+++ ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/data/datasource/datastore/UserPreferencesDataSource.kt
@@ -1,16 +1,11 @@
 package com.training.ecommerce.data.datasource.datastore
 
 import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class UserPreferencesDataSource(private val context: Context) {
-
-    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_preferences")
 
     suspend fun saveLoginState(isLoggedIn: Boolean) {
         context.dataStore.edit { preferences ->
@@ -24,13 +19,11 @@ class UserPreferencesDataSource(private val context: Context) {
         }
     }
 
-    val isUserLoggedIn: Flow<Boolean> = context.dataStore.data
-        .map { preferences ->
+    val isUserLoggedIn: Flow<Boolean> = context.dataStore.data.map { preferences ->
             preferences[DataStoreKeys.IS_USER_LOGGED_IN] ?: false
         }
 
-    val userID: Flow<String?> = context.dataStore.data
-        .map { preferences ->
+    val userID: Flow<String?> = context.dataStore.data.map { preferences ->
             preferences[DataStoreKeys.USER_ID]
         }
 }
