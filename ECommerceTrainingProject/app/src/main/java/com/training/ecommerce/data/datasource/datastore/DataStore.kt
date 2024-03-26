a5958acd0e38ed26fd3122f677967b610b0a9d54
eslamfaisal <eslam.faisal@algodriven.xyz>
2024-03-26 22:03:29 +0200

    log firebase out and catch error

--- ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/data/datasource/datastore/DataStore.kt
+++ ECommerceTrainingProject/app/src/main/java/com/training/ecommerce/data/datasource/datastore/DataStore.kt
@@ -10,19 +10,21 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import com.google.protobuf.InvalidProtocolBufferException
 import com.training.ecommerce.data.datasource.datastore.DataStoreKeys.E_COMMERCE_PREFERENCES
+import com.training.ecommerce.data.datasource.datastore.DataStoreKeys.USER_DETAILS_PREFERENCES_PB
 import com.training.ecommerce.data.models.user.UserDetailsPreferences
 import java.io.InputStream
 import java.io.OutputStream
 
 object DataStoreKeys {
     const val E_COMMERCE_PREFERENCES = "e_commerce_preferences"
+    const val USER_DETAILS_PREFERENCES_PB = "user_details.pb"
     val IS_USER_LOGGED_IN = booleanPreferencesKey("is_user_logged_in")
 }
 
 val Context.appDataStore: DataStore<Preferences> by preferencesDataStore(name = E_COMMERCE_PREFERENCES)
 
 val Context.userDetailsDataStore by dataStore(
-    fileName = "user_details.pb", serializer = UserDetailsPreferencesSerializer
+    fileName = USER_DETAILS_PREFERENCES_PB, serializer = UserDetailsPreferencesSerializer
 )
 
 object UserDetailsPreferencesSerializer : Serializer<UserDetailsPreferences> {
