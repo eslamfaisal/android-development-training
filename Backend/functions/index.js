4394132f2bf9ccfb010e3dd83e5a61ee4a098af7
Eslam <eslamfaisal423@Gmail.com>
2024-05-31 01:16:46 +0300

    get sales by saleType cloud function

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -61,62 +61,64 @@ exports.registerUser = onRequest(async (req, res) => {
     }
 });
 
-exports.getFlashSaleProducts = onRequest(async (req, res) => {
-    let countryID = req.query.country_id;
-    let limit = parseInt(req.query.limit, 10);
-    let lastDocumentId = req.query.last_document;
-
-    if (!countryID) {
-        return res.status(400).send('country_id is required');
-    }
-
-    if (isNaN(limit) || limit <= 0) {
-        return res.status(400).send('limit should be a positive number');
-    }
-
+exports.getSaleProducts = onRequest(async (req, res) => {
     try {
-        let query = admin.firestore().collection('products').limit(limit);
+        const countryID = req.query.country_id;
+        const limit = parseInt(req.query.limit, 10);
+        const lastDocumentId = req.query.last_document;
+        const saleType = req.query.sale_type;
+
+        if (!countryID) {
+            return res.status(400).send('country_id is required');
+        }
+
+        if (isNaN(limit) || limit <= 0) {
+            return res.status(400).send('limit should be a positive number');
+        }
+
+        const productsRef = admin.firestore().collection('products');
+        const productsCollection = productsRef.where('sale_type', '==', saleType);
+        let productsQuery = productsCollection.limit(limit);
 
         if (lastDocumentId) {
-            const lastDocumentSnapshot = await admin.firestore().collection('products').doc(lastDocumentId).get();
-            if (lastDocumentSnapshot.exists) {
-                query = query.startAfter(lastDocumentSnapshot);
-            } else {
+            const lastDocumentSnapshot = await productsRef.doc(lastDocumentId).get();
+            if (!lastDocumentSnapshot.exists) {
                 return res.status(400).send('Invalid last_document ID');
             }
+            productsQuery = productsQuery.startAfter(lastDocumentSnapshot);
         }
 
-        const snapshot = await query.get();
+        const productsSnapshot = await productsQuery.get();
 
-        if (snapshot.empty) {
-            return res.status(200).send({
-                products: [],
-                offers: []
-            });
+        if (productsSnapshot.empty) {
+            return res.status(200).send({products: []});
         }
 
-        let ids = snapshot.docs.map(doc => doc.id);
+        const productIds = productsSnapshot.docs.map(doc => doc.id);
 
-        const offersSnapshot = await admin.firestore().collection('product_offers').where('countries', 'array-contains-any', [countryID]).where('product_id', 'in', ids).get();
+        const offersSnapshot = await admin.firestore().collection('product_offers')
+            .where('countries', 'array-contains-any', [countryID])
+            .where('product_id', 'in', productIds)
+            .get();
 
-
-        let products = snapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
-
-        let offers = offersSnapshot.docs.map(doc => ({id: doc.id, ...doc.data()}));
+        const products = productsSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        const offers = offersSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
 
         products.forEach(product => {
-            let offer = offers.find(offer => offer.product_id === product.id);
+            const offer = offers.find(offer => offer.product_id === product.id);
             if (offer) {
-                product.offer_percentage = offer.countries_offers.find(offer => offer.country_id === countryID).offer_percentage;
+                const countryOffer = offer.countries_offers.find(offer => offer.country_id === countryID);
+                if (countryOffer) {
+                    product.offer_percentage = countryOffer.offer_percentage;
+                }
             }
         });
 
-        res.status(200).send({
-            products: products
-        });
+        res.status(200).send({ products });
 
     } catch (error) {
         console.error('Error getting flash sale products:', error);
         res.status(500).send('Error getting flash sale products: ' + error.message);
     }
 });
+
